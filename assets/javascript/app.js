f46a667736c94227ff59994c9abb3cddc74ab494
njorogepeter <pgakonde@yahoo.com>
2019-01-31 14:49:50 -0800

    javascript

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,3 +8,32 @@
     messagingSenderId: "524072041156"
   };
   firebase.initializeApp(config);
+
+  // database variable
+  var database = firebase.database();
+
+
+  $("#add-tain-btn").on("click", function(){
+
+    var trainName = $("#train-name-input").val().trim();
+    var destination = $("#destination-input").val().trim();
+    var firstTrain = $("#first-train-input").val().trim();
+    var frequency = $("#frequency-input").val().trim();
+
+
+    var newTrain = {
+
+        name: trainName,
+        destination: destination,
+        firstTrain: firstTrain,
+        frequency: frequency
+    };
+
+    database.ref().push(newTrain);
+
+    console.log(newTrain.name);
+    console.log(newTrain.destination);
+    console.log(newTrain.firstTrain);
+    console.log(newTrain.frequency);
+
+  });
