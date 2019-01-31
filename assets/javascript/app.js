b80b84f21ccbf89266d31fcd22d437e9035e6755
njorogepeter <pgakonde@yahoo.com>
2019-01-31 14:16:52 -0800

    beginning stages

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+// initialize firebase
+  var config = {
+    apiKey: "AIzaSyCdKHUEOt9FTLNFJdUoPiODJJNC3Ud1w-M",
+    authDomain: "trainscheduler-3e452.firebaseapp.com",
+    databaseURL: "https://trainscheduler-3e452.firebaseio.com",
+    projectId: "trainscheduler-3e452",
+    storageBucket: "trainscheduler-3e452.appspot.com",
+    messagingSenderId: "524072041156"
+  };
+  firebase.initializeApp(config);
