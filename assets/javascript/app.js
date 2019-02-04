19d4555c8c712d7b5bfef7e8400f8697d31ec320
njorogepeter <pgakonde@yahoo.com>
2019-02-03 22:30:21 -0800

    additional changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,8 +12,8 @@
   // database variable
   var database = firebase.database();
 
-
-  $("#add-tain-btn").on("click", function(){
+  // whenever user clicks add button
+  $("#add-train-btn").on("click", function(){
 
     var trainName = $("#train-name-input").val().trim();
     var destination = $("#destination-input").val().trim();
@@ -29,11 +29,66 @@
         frequency: frequency
     };
 
+    // Firebase watcher + pushes train data into database upon clicking add button on webpage
     database.ref().push(newTrain);
 
     console.log(newTrain.name);
     console.log(newTrain.destination);
     console.log(newTrain.firstTrain);
     console.log(newTrain.frequency);
+
+    $("#train-name-input").val("");
+    $("#destination-input").val("");
+    $("#first-train-input").val("");
+    $("frequency-input").val("");
+
+    //checks for next train arrival
+    return false; 
+
+  });
+  //retrieves train data from the database and populates into table
+  database.ref().on("child_added", function(snap){
+
+    console.log(snap.val().name);
+
+    var val = snap.val();
+  
+    // Store everything into a variable.
+    var name = val.name;
+    var destination = val.destination;
+    var frequency = val.frequency;
+    var firstTrain = val.firstTrain;
+
+    // First Time
+    var timeArr = firstTrain.split(":");
+    var trainTime = moment().hours(timeArr[0]).minutes(timeArr[1]);
+    var maxMoment = moment.max(moment(), trainTime);
+    var tMinutes;
+    var tArrival;
+
+    if(maxMoment === trainTime){
+        tArrival = trainTime.format("hh:mm A");
+        tMinutes = trainTime.diff(moment(), "minutes");
+
+    } else {
+        // Difference between the times
+        var differenceTimes = moment().diff(trainTime, "minutes");
+        
+        // Train apart (remainder)
+        var tRemainder = differenceTimes % frequency;
+        
+        // Minute Until Train
+        tMinutes = frequency - tRemainder;
+        
+        // Next Train
+        tArrival = moment().add(tMinutes, "m").format("hh:mm A");
+    }
+
+    console.log("tMinutes:", tMinutes);
+    console.log("tArrival:", tArrival);
+
+
+    $("#train-table > tbody").append("<tr><td>" + name + "</td><td>" + destination + "</td><td>" + frequency + "</td><td>"
+    + tArrival + "</td><td>" + tMinutes + "</td></tr>");
 
   });
